e8c771a5196638310a2923b396a058ea0d844596
EchoCow <lizhongyue248@163.com>
2019-08-07 11:43:04 +0800

    :sparkles: Add home api.

--- src/main/java/cn/edu/gzmu/authserver/controller/HomeController.java
+++ src/main/java/cn/edu/gzmu/authserver/controller/HomeController.java
@@ -3,6 +3,7 @@ package cn.edu.gzmu.authserver.controller;
 import com.alibaba.fastjson.JSONObject;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.provider.endpoint.FrameworkEndpointHandlerMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 /**
@@ -22,23 +24,35 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class HomeController {
 
+    private final @NonNull HttpServletRequest request;
     private final @NonNull RequestMappingHandlerMapping requestMappingHandlerMapping;
     private final @NonNull FrameworkEndpointHandlerMapping frameworkEndpointHandlerMapping;
 
     @RequestMapping("/")
-    public HttpEntity<?> home(){
+    public HttpEntity<?> home() {
         Map<RequestMappingInfo, HandlerMethod> endpointHandlerMethods = frameworkEndpointHandlerMapping.getHandlerMethods();
         Map<RequestMappingInfo, HandlerMethod> applicationHandlerMethods = requestMappingHandlerMapping.getHandlerMethods();
         JSONObject result = new JSONObject();
-        endpointHandlerMethods.forEach((key, value)->
-                result.put(value.getMethod().getName(), key.getPatternsCondition()
-                        .getPatterns().stream()
-                        .findFirst().orElse("")));
-        applicationHandlerMethods.forEach((key, value)->
-                result.put(value.getMethod().getName(), key.getPatternsCondition()
-                        .getPatterns().stream()
-                        .findFirst().orElse("")));
+        endpointHandlerMethods.forEach((key, value) ->
+                result.put(value.getMethod().getName(), getBaseUrl() + getPath(key)));
+        applicationHandlerMethods.forEach((key, value) ->
+                result.put(value.getMethod().getName(), getBaseUrl() + getPath(key)));
         return ResponseEntity.ok(result);
     }
 
+    private String getBaseUrl() {
+        StringBuffer url = request.getRequestURL();
+        if (StringUtils.endsWith(url, "/")) {
+            return StringUtils.substringBeforeLast(url.toString(), "/");
+        }
+        return url.toString();
+    }
+
+    private String getPath(RequestMappingInfo requestMappingInfo) {
+        return requestMappingInfo.getPatternsCondition()
+                .getPatterns()
+                .stream()
+                .findFirst()
+                .orElse("");
+    }
 }
