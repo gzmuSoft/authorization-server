6c7a434adf8ea46a9cfe2d73647ae75cc27e8829
EchoCow <lizhongyue248@163.com>
2020-01-05 14:50:04 +0800

    :sparkles: Fix jwt info

--- src/main/java/cn/edu/gzmu/authserver/auth/AuthTokenEnhancer.java
+++ src/main/java/cn/edu/gzmu/authserver/auth/AuthTokenEnhancer.java
@@ -2,7 +2,6 @@ package cn.edu.gzmu.authserver.auth;
 
 import cn.edu.gzmu.authserver.model.entity.SysUser;
 import cn.edu.gzmu.authserver.repository.SysUserRepository;
-import cn.edu.gzmu.authserver.service.SysRoleService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
@@ -29,18 +28,22 @@ import java.util.stream.Collectors;
 public class AuthTokenEnhancer implements TokenEnhancer {
 
     private final @NonNull SysUserRepository sysUserRepository;
-    private final @NonNull SysRoleService sysRoleService;
 
     @Override
     public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
         User user = (User) authentication.getPrincipal();
-        final Map<String, Object> additionalInfo = new HashMap<>(2);
+        final Map<String, Object> additionalInfo = new HashMap<>(6);
         SysUser sysUser = sysUserRepository.findFirstByName(user.getUsername()).orElseThrow(
                 () -> new UsernameNotFoundException("用户未找到！")
         );
         additionalInfo.put("user_name", user.getUsername());
         additionalInfo.put("authorities", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()));
+        additionalInfo.put("sub", user.getUsername());
         additionalInfo.put("user_id", sysUser.getId());
+        // A timestamp indicating when the token was issued
+        additionalInfo.put("iat", (System.currentTimeMillis()) / 1000L);
+        // A timestamp indicating when the token is not to be used before
+        additionalInfo.put("nbf", (System.currentTimeMillis()) / 1000L + accessToken.getExpiresIn());
         ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
         return accessToken;
     }
