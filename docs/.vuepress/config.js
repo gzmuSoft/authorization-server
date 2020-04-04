cad07b9b1b584a7604b161f8739e5b56c9a39534
EchoCow <lizhongyue248@163.com>
2020-04-04 19:03:18 +0800

    :memo: Add docs.

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,44 @@
+module.exports = {
+    title: '授权服务器',
+    description: '文档说明',
+    port: 1111,
+    base: '/authorization-server/',
+    locales: {
+        '/': {lang: 'zh-CN'}
+    },
+    head: [
+        ['link', {rel: 'apple-touch-icon', href: '/apple-touch-icon.png'}],
+        ['link', {rel: 'icon', href: '/favicon-32x32.png'}],
+        ['link', {rel: 'manifest', href: '/manifest.json'}],
+        ['meta', {name: 'theme-color', content: '#ffffff'}]
+    ],
+    plugins: {
+        '@vuepress/pwa': {
+            serviceWorker: true,
+            updatePopup: {
+                message: '内容已经更新啦～',
+                buttonText: '跟我走！'
+            }
+        },
+        '@vuepress/back-to-top': true
+    },
+    themeConfig: {
+        repo: 'gzmuSoft/authorization-server',
+        editLinkText: '帮助我们改进此文档！',
+        editLinks: true,
+        docsDir: 'docs',
+        nav: [
+            {text: '主页', link: '/'},
+            {text: '操作手册', link: '/description/'},
+            {text: '技术架构', link: '/technology/'}
+        ],
+        sidebar: {
+            '/description/': [
+                ['', '文档说明'],
+                ['/description/OAuth2', 'OAuth2']
+            ]
+        },
+        sidebarDepth: 2,
+        lastUpdated: 'Last Updated'
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: /index.png
+actionText: 快速阅览 →
+actionLink: /description/
+features:
+- title: oauth 2 多种模式授权
+  details: 更加安全完整的授权模式，具有高扩展性，第三方授权极其容易，提供多种授权模式支持。
+- title: RBAC 动态授权
+  details: 所有资源通过角色进行灵活控制，自己管理应用权限，提供基于角色动态资源权限控制。
+- title: SSO、JWT 支持
+  details: 提供 SSO 支持，多个应用一键登录，前后端分离，使用 JWT 令牌非对称密钥加密，具有高安全性。
+footer: MIT Licensed | Copyright © 2020-present GZMU Echo Cow
+---
