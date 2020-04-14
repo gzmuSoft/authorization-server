f9bae67ff10ea3c58a997946971708f285565eb0
EchoCow <lizhongyue248@163.com>
2020-04-14 22:47:36 +0800

    :sparkles: Add sys data api.

--- src/main/java/cn/edu/gzmu/authserver/repository/SysDataRepository.java
+++ src/main/java/cn/edu/gzmu/authserver/repository/SysDataRepository.java
@@ -1,10 +1,12 @@
 package cn.edu.gzmu.authserver.repository;
 
-
 import cn.edu.gzmu.authserver.base.BaseRepository;
 import cn.edu.gzmu.authserver.model.constant.AuthConstant;
 import cn.edu.gzmu.authserver.model.entity.SysData;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 /**
  * SysData Repository
@@ -15,5 +17,15 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  */
 @RepositoryRestResource(path = AuthConstant.SYS_DATA)
 public interface SysDataRepository extends BaseRepository<SysData, Long> {
+
+    /**
+     * 通过父级id和类型查询
+     *
+     * @param parentId 父级id
+     * @param type 类型
+     * @return 结果
+     */
+    @RestResource(path = "/byParentIdAndType")
+    List<SysData> findAllByParentIdAndType(Long parentId, Integer type);
 
 }
